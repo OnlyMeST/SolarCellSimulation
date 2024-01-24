dda675de5807405f9e4b70b80e91172366779b22
Airis <104155192+OnlyMeST@users.noreply.github.com>
2024-01-24 10:35:42 +0100

    Update SolarEnergyEntity.java

--- solarcell/src/main/java/com/saria/solarcell/solarcomponents/SolarEnergyEntity.java
+++ solarcell/src/main/java/com/saria/solarcell/solarcomponents/SolarEnergyEntity.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "solar_cell")
+@Table(name = "your_table")
 public class SolarEnergyEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
