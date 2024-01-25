6dc57431e605e46d156289d121757753017f3671
Airis <104155192+OnlyMeST@users.noreply.github.com>
2024-01-25 09:50:59 +0100

    Update SolarPanelRepository.java

--- solarcell/src/main/java/com/saria/solarcell/solarcomponents/SolarPanelRepository.java
+++ solarcell/src/main/java/com/saria/solarcell/solarcomponents/SolarPanelRepository.java
@@ -1,14 +1,15 @@
 package com.saria.solarcell.solarcomponents;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface SolarPanelRepository extends JpaRepository<SolarEnergyEntity, Long> {
-    
+	List<SolarEnergyEntity> findById(Integer id);
+	List<SolarEnergyEntity> findByIdBetween(Integer startId, Integer endId);
     List<SolarEnergyEntity> findBySolarIrradiance(double solarIrradiance);
     List<SolarEnergyEntity> findByPanelEfficiency(double panelEfficiency);
     List<SolarEnergyEntity> findByPanelArea(double panelArea);
@@ -17,6 +18,7 @@ public interface SolarPanelRepository extends JpaRepository<SolarEnergyEntity, L
     List<SolarEnergyEntity> findBySoilingLossFactor(double soilingLossFactor);
     List<SolarEnergyEntity> findByMismatchLossFactor(double mismatchLossFactor);
     List<SolarEnergyEntity> findByTimestamp(LocalDateTime timestamp);
+    List<SolarEnergyEntity> findByTimestampLessThanEqual(LocalDateTime timestamp);
     List<SolarEnergyEntity> findByEnergyOutput(double energyOutput);
     List<SolarEnergyEntity> findByNetEnergy(double netEnergy);
 }
